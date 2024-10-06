c745741ce4873783025effae3fdd92f5c1ad4bcc
A.M2015 <amirhoseinmomeni2015@gmail.com>
2024-10-06 12:00:10 +0330

    Levels
    Update next & Pre Level Butoon Script

--- Assets/Scripts/NextPreviousLevel.cs
+++ Assets/Scripts/NextPreviousLevel.cs
@@ -5,18 +5,60 @@ using UnityEngine.SceneManagement;
 public class NextPreviousLevel : MonoBehaviour
 {
 
+	int totalScenes = 45;
+
+
 	public void Next_Level()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		//active scene
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+		//check if final level
+		if (currentSceneIndex < totalScenes - 1)
+		{
+		
+			SceneManager.LoadScene(currentSceneIndex + 1);
+		}
+		else
+		{
+			
+			Debug.Log("The End");
+		
+		}
+
+
+
 	}
+
+
+
+
+
+
+//----------------------------------------
+
+
 
 
 	public void pre_Level()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		if (currentSceneIndex > 0)
+		{
+			
+			SceneManager.LoadScene(currentSceneIndex - 1);
+		}
+		else
+		{
+			
+			Debug.Log("first level");
+		
+		}
+
+
 	}
 
-
+//-------------------------------------------------
 
 
 	public void Rsest_Level()
